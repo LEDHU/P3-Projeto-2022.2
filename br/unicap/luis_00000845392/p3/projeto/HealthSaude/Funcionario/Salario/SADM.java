7f81439da4bd4c225d6c7c2b3354a29f50dc24ae
Eduardo Peixoto <luis.00000845392@unicap.br>
2022-10-23 19:52:55 -0300

    Renomiado

--- br/unicap/luis_00000845392/p3/projeto/HealthSaude/Funcionario/Salario/SADM.java
+++ br/unicap/luis_00000845392/p3/projeto/HealthSaude/Funcionario/Salario/SADM.java
@@ -1,8 +1,9 @@
 package br.unicap.luis_00000845392.p3.projeto.HealthSaude.Funcionario.Salario;
 
-public class ISRecepcionista implements ISalario{
+public class SADM implements ISalario{
     @Override
     public double calcularSalario(double extra) {
-        return 3000 + 100*extra;
+        return 4000 + 100*extra;
     }
+
 }
--- br/unicap/luis_00000845392/p3/projeto/HealthSaude/Funcionario/Salario/SMedico.java
+++ br/unicap/luis_00000845392/p3/projeto/HealthSaude/Funcionario/Salario/SMedico.java
@@ -1,6 +1,6 @@
 package br.unicap.luis_00000845392.p3.projeto.HealthSaude.Funcionario.Salario;
 
-public class ISMedico implements ISalario{
+public class SMedico implements ISalario{
     @Override
     public double calcularSalario(double extra) {
         return 5000 + 100*extra;
