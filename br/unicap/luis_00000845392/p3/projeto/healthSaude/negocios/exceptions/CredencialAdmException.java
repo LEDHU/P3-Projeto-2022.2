0955f182e81348785ba176324e4265f23ebf6e0f
Eduardo Peixoto <luis.00000845392@unicap.br>
2022-11-28 10:12:52 -0300

    apresentacao 28/11

--- br/unicap/luis_00000845392/p3/projeto/healthSaude/negocios/exceptions/CredencialAdmException.java
+++
@@ -1,7 +0,0 @@
-package br.unicap.luis_00000845392.p3.projeto.healthSaude.negocios.exceptions;
-
-public class CredencialAdmException extends Exception{
-    public CredencialAdmException() {
-        super("ERRO - Não exite credencial para ADM");
-    }
-}
--- br/unicap/luis_00000845392/p3/projeto/healthSaude/negocios/exceptions/CredencialMedicoException.java
+++
@@ -1,8 +0,0 @@
-package br.unicap.luis_00000845392.p3.projeto.healthSaude.negocios.exceptions;
-
-public class CredencialMedicoException extends Exception{
-
-    public CredencialMedicoException() {
-        super("ERRO - Não exite credencial para Medico");
-    }
-}
